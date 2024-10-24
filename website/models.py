bb7377e8b1b52f48893fb079b88a8de65e7a9a91
Derek <reign.derek@yahoo.com>
2024-10-24 13:59:01 +0000

    Add `TeamMember` model and data

--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
 # from django.db import models
 
 # Create your models here.
+from dataclasses import dataclass
+from typing import List, Optional
+import json
+
+@dataclass
+class TeamMember:
+    name: str
+    email: str
+    team: str
+    role: str
+    picture: Optional[str] = None
+
+    @classmethod
+    def load_from_json(cls, filepath: str) -> List["TeamMember"]:
+        with open(filepath, 'r') as f:
+            team_members_data = json.load(f)
+
+        return [cls(**member) for member in team_members_data]
