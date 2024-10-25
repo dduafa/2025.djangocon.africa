99bc17b12122d838f7eb2b43d99fbd21d273bed8
Derek <reign.derek@yahoo.com>
2024-10-25 10:30:51 +0000

    Add team page

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path("", views.page_home, name="page_home"),
+    path("team/", views.page_team, name="page_team"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from website.models import TeamMember
 
 
 def page_home(request):
     return render(request, "website/page_home.html")
+
+
+def page_team(request):
+    team_members = TeamMember.load_from_json('website/team.json')
+    return render(request, "website/page_team.html", {"members": team_members})
